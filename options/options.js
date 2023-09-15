5f350e8a09e5c6332b420f562943a22d9143dee7
Ravinder QA Lead <ravinder.y@crowdbotics.com>
2023-09-15 13:03:38 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "testingdevstring-d-116414",
   url: "https://testingdevstring-d-116414.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
