d8c7ee7a3bfccc882f56aa866b725c451f46bfca
Ravinder QA Lead <ravinder.y@crowdbotics.com>
2023-10-05 13:28:46 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI2/index.js
+++ screens/ScreenAI2/index.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet, SafeAreaView } from 'react-native';
+import React from "react";
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet, SafeAreaView } from "react-native";
 
-const LoginScreen = () => {
+const SignUpScreen = () => {
   return <SafeAreaView style={styles.container}>
       <View style={styles.logoContainer}>
         <Image source={{
-        uri: 'https://tinyurl.com/42evm3m3'
+        uri: "https://tinyurl.com/42evm3m3"
       }} style={styles.logo} />
         <Text style={styles.logoText}>My App</Text>
       </View>
       <View style={styles.formContainer}>
+        <TextInput placeholder="Full Name" style={styles.input} />
         <TextInput placeholder="Email" style={styles.input} />
         <TextInput placeholder="Password" secureTextEntry={true} style={styles.input} />
+        <TextInput placeholder="Confirm Password" secureTextEntry={true} style={styles.input} />
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Login</Text>
+          <Text style={styles.buttonText}>Sign Up</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.forgotPassword}>
-          <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+        <TouchableOpacity style={styles.signIn}>
+          <Text style={styles.signInText}>
+            Already have an account? Sign In
+          </Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>;
@@ -25,12 +29,12 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingHorizontal: 20,
-    justifyContent: 'center'
+    justifyContent: "center"
   },
   logoContainer: {
-    alignItems: 'center'
+    alignItems: "center"
   },
   logo: {
     width: 100,
@@ -39,36 +43,36 @@ const styles = StyleSheet.create({
   },
   logoText: {
     fontSize: 20,
-    fontWeight: 'bold'
+    fontWeight: "bold"
   },
   formContainer: {
     marginTop: 50
   },
   input: {
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 5,
     padding: 10,
     marginBottom: 20
   },
   button: {
-    backgroundColor: '#007AFF',
+    backgroundColor: "#007AFF",
     padding: 15,
     borderRadius: 5,
-    alignItems: 'center'
+    alignItems: "center"
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: "bold"
   },
-  forgotPassword: {
-    alignItems: 'center',
+  signIn: {
+    alignItems: "center",
     marginTop: 20
   },
-  forgotPasswordText: {
-    color: '#007AFF',
+  signInText: {
+    color: "#007AFF",
     fontSize: 16
   }
 });
-export default LoginScreen;
+export default SignUpScreen;
